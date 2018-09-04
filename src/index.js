2c7583b808c42573795cdd48ff1c21c79b70d99c
rdm100 <robertode_marco@hotmail.com>
2018-09-04 20:35:18 +0100

    .debounce() added to search bar so videos will change when every 300 ms when someone types in the search bar

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import YouTube from 'youtube-api-search';
@@ -32,9 +33,12 @@ class App extends Component {
 	}
 
 	render() {
+		//.debounce()takes the inner function and returns a new function that can only be called every 300 ms
+		const videoSearch = _.debounce(term => this.videoSearch(term), 300); 
+
 			return (
 	     <div>
-	     	<SearchBar onSearchTermChange={term => this.videoSearch(term)} />
+	     	<SearchBar onSearchTermChange={videoSearch} />
 	     	<VideoDetail video={this.state.selectedVideo} />
 	     	<VideoList 
 	     	onVideoSelect={selectedVideo => this.setState({selectedVideo})}
